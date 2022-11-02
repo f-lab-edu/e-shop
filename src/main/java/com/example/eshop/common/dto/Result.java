b5a261618a10cf33d4c70e504ef1e2af29522413
dev-hjkim <dev--test@naver.com>
2022-11-03 00:26:07 +0900

    add restControllerAdvice

--- src/main/java/com/example/eshop/common/dto/Result.java
+++ src/main/java/com/example/eshop/common/dto/Result.java
@@ -1,8 +1,10 @@
 package com.example.eshop.common.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Result {
